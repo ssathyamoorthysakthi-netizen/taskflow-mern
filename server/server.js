19fbb48b5c3e559262bf29bfbd43fdce64b9c5fb
ssathyamoorthysakthi-netizen <ssathyamoorthysakthi-netizen@users.noreply.github.com>
2026-09-23 21:37:41 +0530

    fix: default client API baseURL to deployed Render backend in production builds

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Welcome to TaskFlow API', version: '1.0.0' });
 });
 
